4b20a380e401575d77362a3093d4342d8a87539e
LT-Phu <phub2014686@student.ctu.edu.vn>
2023-11-01 13:31:22 +0700

    Tao trang cap nhat lien he

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,22 @@
-<script >
+<script>
 import AppHeader from "@/components/AppHeader.vue";
-
 export default {
   components: {
     AppHeader,
   },
-}
+};
 </script>
 
 <template>
-    <div id="app">
-      <AppHeader />
-      <div class="container mt-3">
-        <router-view />
-      </div>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
     </div>
+  </div>
 </template>
 
-<style>
+<style scoped>
 .page {
   max-width: 400px;
   margin: auto;
